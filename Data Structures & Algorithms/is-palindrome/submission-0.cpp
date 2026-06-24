51df79fd4d36f6dcd4e1cc247e467aa6a5432c31
raabiya2025 <rabiyanida2024@gmail.com>
2026-06-24 14:45:37 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int n=s.size();
+        int right=n-1;
+        
+        for(int i=0;i<n-1;i++){
+            if(!isalnum(s[left])){
+                left++;
+                continue;
+            }
+            if(!isalnum(s[right])){
+                right--;
+                continue;
+            }
+            if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
